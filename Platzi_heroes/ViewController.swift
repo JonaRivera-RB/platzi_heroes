439a21fa8cb3a1fed1c7ba54c4b631a5e1ae2300
Jonathan Rivera <jonathan@vetta.io>
2021-01-05 11:58:02 -0600

    Initial Commit

---
+++ Platzi_heroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Platzi_heroes
+//
+//  Created by Jonathan Rivera Misael on 05/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
